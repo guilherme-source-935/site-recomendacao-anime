8012b7cb8f61fe2c9c6eb7c940bb5e39c5ffeb51
Guilherme <guilhermesilvabarth@gmail.com>
2026-07-11 18:11:04 -0300

    feat: CSS da landing page criado

---
+++ js/components/carousel.js
@@ -0,0 +1,4 @@
+// const classes = document.querySelectorAll(".carousel-lista")
+// classes.forEach(function(elemento, i) {
+//     elemento.style.setProperty("--carousel-id", `--carousel-${i}`)
+// });
