1b3818f7e460210a0108598dce8fab4b237b090c
Guilherme <guilhermesilvabarth@gmail.com>
2026-07-18 01:09:03 -0300

    API do AniList integrada

--- js/components/carousel.js
+++ js/components/carousel.js
@@ -1,15 +1,17 @@
-let carouselWidth = $("#carousel-novidades .carousel-inner")[0].scrollWidth
-let cardWidth = $("#carousel-novidades .carousel-item").width()
-let scrollPosicao = 0
-$("#carousel-novidades .carousel-control-next").on("click", function () {
-    if (scrollPosicao < (carouselWidth - cardWidth * 4)) {
-        scrollPosicao += cardWidth
-        $("#carousel-novidades .carousel-inner").animate({ scrollLeft: scrollPosicao }, 600)
-    }
-});
-$("#carousel-novidades .carousel-control-prev").on("click", function () {
-    if (scrollPosicao > 0) {
-        scrollPosicao -= cardWidth
-        $("#carousel-novidades .carousel-inner").animate({ scrollLeft: scrollPosicao }, 600)
-    }
-});
+function inicializarCarousel(){
+    let carouselWidth = $("#carousel-novidades .carousel-inner")[0].scrollWidth
+    let cardWidth = $("#carousel-novidades .carousel-item").width()
+    let scrollPosicao = 0
+    $("#carousel-novidades .carousel-control-next").on("click", function () {
+        if (scrollPosicao < (carouselWidth - cardWidth * 4)) {
+            scrollPosicao += cardWidth
+            $("#carousel-novidades .carousel-inner").animate({ scrollLeft: scrollPosicao }, 600)
+        }
+    });
+    $("#carousel-novidades .carousel-control-prev").on("click", function () {
+        if (scrollPosicao > 0) {
+            scrollPosicao -= cardWidth
+            $("#carousel-novidades .carousel-inner").animate({ scrollLeft: scrollPosicao }, 600)
+        }
+    });
+}
